81e4e8f70213df9e8bc394982f64945c6306c1e8
Tim Furlong <furlong.tim@gmail.com>
2014-02-13 17:47:59 -0500

    Getting started with Heroku: https://devcenter.heroku.com/articles/getting-started-with-django

--- FridgeRaiderProject/wsgi.py
+++ FridgeRaiderProject/wsgi.py
@@ -26,3 +26,12 @@ application = get_wsgi_application()
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
+
+# ==============================================================================
+# From https://devcenter.heroku.com/articles/getting-started-with-django
+# ==============================================================================
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
