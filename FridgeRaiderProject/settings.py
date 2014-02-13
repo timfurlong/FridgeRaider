307b8138678982eae1fb5e3d3d340c18a63645d7
Tim Furlong <furlong.tim@gmail.com>
2014-02-13 03:16:18 -0500

    Changed DB settings.

--- FridgeRaiderProject/settings.py
+++ FridgeRaiderProject/settings.py
@@ -14,7 +14,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'FridgeRaiderProject',   # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': '50dollors',                  # Not used with sqlite3.
+        'PASSWORD': 'waitforit',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
